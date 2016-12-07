a2125cdfec10a5b5ac52e452e90ad40c73dff585
Sebastian Senf <contact@mustardamus.com>
2016-12-07 16:58:53 +0100

    added the config example

---
+++ server/config/auth.example.js
@@ -0,0 +1,14 @@
+// remove '.example' from filename to make it work (auth.js is not in version control)
+'use strict'
+
+module.exports = {
+  // the password to encrypt the data
+  password: 'TODO: have the template after hook generate a random password',
+
+  // github application information
+  githubClientId: 'register an oauth app at',
+  githubClientSecret: 'https://github.com/settings/developers',
+
+  // name of the cookie
+  cookieName: 'appname-cookie'
+}
